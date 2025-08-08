05a519e4f78988ccbf8558ba020083d6dbb4fef1
Kenny Ruici <kennyruici123@gmail.com>
2025-08-08 16:28:52 +0700

    adjust api endpoint

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get('http://localhost:5555/books')
+      .get('https://bookstore-mern-ipy5.vercel.app')
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
