e16ffab97108b1e5e9c5876ee3479404b3fc8998
Kenny <kenny@aiteknologi.com>
2025-08-11 11:51:28 +0700

    constring add db name

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://ruicikenny:OIi1kfaMf042yrU9@cluster0.cqd2a3z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  'mongodb+srv://ruicikenny:OIi1kfaMf042yrU9@cluster0.cqd2a3z.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Cluster0';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
